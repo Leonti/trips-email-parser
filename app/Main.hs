d5ccd41fe8f8cfaa7171b812e191c4ed774bf005
Leonti Bielski <lbielski@seek.com.au>
2019-10-31 13:13:01 +1100

    Experiments with megaparsec

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,26 @@ module Main where
 
 import Lib
 
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void
+
+type Parser = Parsec Void String
+
+test :: IO ()
+test = parseTest (satisfy (== 'a') :: Parser Char) ""
+
+-- parseTest (string' "foo" :: Parser String) "FOO"
+
+mySequence :: Parser (Char, Char, Char)
+mySequence = do
+  a <- char 'a'
+  b <- char 'b'
+  c <- char 'c'
+  return (a, b, c)
+
+-- https://markkarpov.com/megaparsec/megaparsec.html#white-space
+
 main :: IO ()
 main = do
   content <- readFile "rawText.txt"
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,23 @@ module Lib
     ( someFunc
     ) where
 
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void
+
+type Parser = Parsec Void String
+
+test :: IO ()
+test = parseTest (satisfy (== 'a') :: Parser Char) ""
+
+-- parseTest (string' "foo" :: Parser String) "FOO"
+
+mySequence :: Parser (Char, Char, Char)
+mySequence = do
+  a <- char 'a'
+  b <- char 'b'
+  c <- char 'c'
+  return (a, b, c)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
