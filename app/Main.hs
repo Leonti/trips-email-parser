4a3bd1ee1e6fa3a06aad0e1d12767d1039c559e7
Leonti Bielski <lbielski@seek.com.au>
2019-11-20 10:12:59 +1100

    Add more experiments

--- app/Main.hs
+++ app/Main.hs
@@ -4,9 +4,19 @@ import Lib
 
 import Text.Megaparsec
 import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
 import Data.Void
 
 type Parser = Parsec Void String
+
+sc :: Parser ()
+sc = L.space space1 empty empty
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: String -> Parser String
+symbol = L.symbol sc
 
 test :: IO ()
 test = parseTest (satisfy (== 'a') :: Parser Char) ""
@@ -21,6 +31,7 @@ mySequence = do
   return (a, b, c)
 
 -- https://markkarpov.com/megaparsec/megaparsec.html#white-space
+-- https://www.reddit.com/r/haskell/comments/7jbfuo/megaparsec_and_lexers_im_missing_a_concept/
 
 main :: IO ()
 main = do
